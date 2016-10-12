bcc1a30b9cdc0d2fd0d73391496db517212f567d
Jibola Sogbein <Jibola@Grace.local>
2016-10-12 18:10:04 +0100

    Edit server start message

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var app = require('./app.js');
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function(err){
   if(err){
     return err;
   }
-  console.log('Tango server started on port %s',port);
+  console.log("Server started at http://localhost:" + port)
 });
