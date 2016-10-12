7fd99ad24d4452f9d129d46ac7e00e140fff53a9
Jibola Sogbein <Jibola@Grace.local>
2016-10-12 18:23:10 +0100

    Add npm script to launch app

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var app = require('./app.js');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 
 app.listen(port, function(err){
   if(err){
     return err;
   }
-  console.log("Server started at http://localhost:" + port)
+  console.log("Server started at http://localhost:%s", port)
 });
